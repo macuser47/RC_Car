9751605cce9d3370925bde22c58c8a59713a9e59
Nic Hodlofski <fred-simons47@hotmail.com>
2018-07-04 19:32:17 -0500

    Add multithreading to stream code

--- stream.py
+++ stream.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import requests
 from threading import Thread
+from multiprocessing import Process
 import sys
 
 cam = cv2.VideoCapture(1)
@@ -15,31 +16,34 @@ tick = 0
 start = time.time()
 
 def read():
-    global current_frame, tick
-    while True:
-        _, current_frame = cam.read()
+    return cam.read()[1]
 
-def encode():
-    global current_image, tick
-    while True:
-        current_image = cv2.imencode(".jpg", current_frame)[1].tostring()
-        tick += 1
+def encode(img):
+    encoded_image = cv2.imencode(".jpg", current_frame)[1].tostring()
 
-def post():
-    global tick
-    while True:
+
+def post(img):
         requests.post("http://" + sys.argv[1] + "/mjpg", data=current_image)
         #time.sleep(1.0 / 30)
 
-t = [Thread(target=read), Thread(target=encode), Thread(target=post)]
-for th in t: th.daemon = True
-for th in t: th.start()
 
 while True:
+    start = time.time()
+    img = read()
+    encoded_image = None
+    proc = Process(target=encode, args=(img,))
+    proc.daemon = True
+    proc.start()
+    proc.join()
+
+    if encoded_image == None:
+        raise Exception("Image not encoded")
+
+    t = Thread(target=post, args=(encoded_image,))
+    t.daemon = True
+    t.start()
+
     if (time.time() - start) != 0:
-        fps = float(tick) / (time.time() - start)
+        fps = 1 / (time.time() - start)
     print("FPS: {}".format(fps))
-    if (time.time() - start) > 2.0:
-        start = time.time()
-        tick = 0
 #print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
