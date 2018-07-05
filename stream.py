927d7773c512879ac9ba94f4081c142164745b65
= <nichodlofski@gmail.com>
2018-07-04 20:14:57 -0500

    Fixed more typos because im an idiot

--- stream.py
+++ stream.py
@@ -10,11 +10,6 @@ ret, frame = cam.read()
 
 time.sleep(0.5)
 
-current_frame = frame
-current_image = cv2.imencode(".jpg", current_frame)[1].tostring()
-tick = 0
-start = time.time()
-
 def read():
     return cam.read()[1]
 
@@ -32,11 +27,11 @@ def process(img):
     t.start()
 
 def encode(img, d):
-    d[0] = cv2.imencode(".jpg", current_frame)[1].tostring()
+    d[0] = cv2.imencode(".jpg", img)[1].tostring()
 
 
 def post(img):
-        requests.post("http://" + sys.argv[1] + "/mjpg", data=current_image)
+    requests.post("http://" + sys.argv[1] + "/mjpg", data=img)
         #time.sleep(1.0 / 30)
 
 
